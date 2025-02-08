4eb23c9910427fdb160b9dc9cad9eeaa5ff8b5e3
Dean Kinyua <kinyuadean@gmail.com>
2025-02-08 16:01:35 +0300

    use render_change/1 to simulate a phx-change event

--- test/ranger_web/live/newsletter_live_test.exs
+++ test/ranger_web/live/newsletter_live_test.exs
@@ -2,4 +2,14 @@ defmodule RangerWeb.NewsletterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "simulate a form change event", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/newsletter")
+
+    email = "kinyuadean"
+
+    html = form(view, "#subscribe", %{subscription: %{email: email}}) |> render_change()
+
+    assert html =~ "has invalid format"
+  end
 end
--- test/ranger_web/live/todo_live_test.exs
+++ test/ranger_web/live/todo_live_test.exs
@@ -4,15 +4,17 @@ defmodule RangerWeb.TodoLiveTest do
   import Phoenix.LiveViewTest
 
   test "user can create a new todo", %{conn: conn} do
-    {:ok, view, html} = live(conn, "/todo")
+    {:ok, view, _html} = live(conn, "/todo")
 
     # form(view, selector, form_data \\ %{})
 
     # Realize that a test always starts from a blank canvas unless
     # you specifically seed data in setup blocks at RangerWeb.ConnCase
 
-    form(view, "#add-todo", %{todo: %{body: "Dean Kinyua"}}) |> render_submit()
+    html = form(view, "#add-todo", %{todo: %{body: "Dean Kinyua"}}) |> render_submit()
 
     assert has_element?(view, "[data-role=todo]", "Dean Kinyua")
+
+    # assert html =~ "Dean Kinyua"
   end
 end
