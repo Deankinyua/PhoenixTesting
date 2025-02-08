8896883a8a27d2e06e9a7796455633a8f28af181
Dean Kinyua <kinyuadean@gmail.com>
2025-02-08 09:17:40 +0300

    use open_browser/1 to see html content directly on the browser

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -2,4 +2,25 @@ defmodule RangerWeb.CounterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "test increment", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/counter")
+
+    increment_button = element(view, "#increment")
+
+    render_click(increment_button)
+    open_browser(view)
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  # test "test decrement", %{conn: conn} do
+  #   {:ok, view, _html} = live(conn, "/counter")
+
+  #   decrement_button = element(view, "#decrement")
+
+  #   render_click(decrement_button)
+
+  #   assert has_element?(view, "#count", "-1")
+  # end
 end
